4c31aa89dc322be27dd80875ef0c7b101c9c6de4
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-08-20 22:00:14 -0700

    [exercise 4]

--- long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
+++ long-ride-alerts/src/main/java/org/apache/flink/training/exercises/longrides/LongRidesExercise.java
@@ -21,7 +21,10 @@ package org.apache.flink.training.exercises.longrides;
 import org.apache.flink.annotation.VisibleForTesting;
 import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.TimerService;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
@@ -47,7 +50,9 @@ public class LongRidesExercise {
     private final SourceFunction<TaxiRide> source;
     private final SinkFunction<Long> sink;
 
-    /** Creates a job using the source and sink provided. */
+    /**
+     * Creates a job using the source and sink provided.
+     */
     public LongRidesExercise(SourceFunction<TaxiRide> source, SinkFunction<Long> sink) {
         this.source = source;
         this.sink = sink;
@@ -98,17 +103,58 @@ public class LongRidesExercise {
     @VisibleForTesting
     public static class AlertFunction extends KeyedProcessFunction<Long, TaxiRide, Long> {
 
+        private transient MapState<Long, Long> startTimes;
+        private transient MapState<Long, Long> endTimes;
+
         @Override
         public void open(Configuration config) throws Exception {
-            throw new MissingSolutionException();
+            this.startTimes = getRuntimeContext().getMapState(new MapStateDescriptor<>("startTimes", Long.class, Long.class));
+            this.endTimes = getRuntimeContext().getMapState(new MapStateDescriptor<>("endTimes", Long.class, Long.class));
         }
 
         @Override
         public void processElement(TaxiRide ride, Context context, Collector<Long> out)
-                throws Exception {}
+                throws Exception {
+            long eventTime = ride.getEventTimeMillis();
+            TimerService timerService = context.timerService();
+            if (ride.isStart) {
+                if (eventTime > timerService.currentWatermark()) {
+                    Long endTime = this.endTimes.get(ride.rideId);
+                    if(endTime != null){
+                        if(endTime - eventTime > Duration.ofHours(2).toMillis()) {
+                            out.collect(ride.rideId);
+                        }
+                        this.endTimes.remove(ride.rideId);
+                        timerService.deleteEventTimeTimer(endTime + Duration.ofHours(2).toMillis());
+                    } else {
+                        this.startTimes.put(ride.rideId, eventTime);
+                        timerService.registerEventTimeTimer(eventTime + Duration.ofHours(2).toMillis());
+                    }
+                }
+            } else {
+                Long startTime = this.startTimes.get(ride.rideId);
+                if (startTime != null) {
+                    if (eventTime - startTime > Duration.ofHours(2).toMillis()) {
+                        // more than 2 hours
+                        out.collect(ride.rideId);
+                    }
+                    this.startTimes.remove(ride.rideId);
+                    timerService.deleteEventTimeTimer(startTime + Duration.ofHours(2).toMillis());
+                } else {
+                    this.endTimes.put(ride.rideId, eventTime);
+                    timerService.registerEventTimeTimer(eventTime + Duration.ofHours(2).toMillis());
+                }
+            }
+        }
 
         @Override
         public void onTimer(long timestamp, OnTimerContext context, Collector<Long> out)
-                throws Exception {}
+                throws Exception {
+            if (this.startTimes.contains(context.getCurrentKey())) {
+                out.collect(context.getCurrentKey());
+            }
+            this.startTimes.remove(context.getCurrentKey());
+            this.endTimes.remove(context.getCurrentKey());
+        }
     }
 }
